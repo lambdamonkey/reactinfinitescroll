3ca22e8fd59daf3f363c5104602984030f1b5618
Evgeniy Smirniy <smirniyh@gmail.com>
2019-02-18 21:30:43 +0200

    Infinite scroll app

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+	APPLICATION_ID: process.env.APPLICATION_ID || '77954f02728648670374888fbf319f4c2bae4c7884f0772cce6f2ded1b3e749d',
+	SECRET: process.env.SECRET || '9bf3d86b5fe8b56ada44bb62991abf8a23577c9e13497cd61f596be65fb72a20',
+	CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+}
